45361aa6588e970cc9be932aea8d002d93cfbb48
27elijahl <27elijahl@students.harker.org>
2025-07-14 19:50:34 -0700

    Added removed TODO message

--- src/main/java/frc/robot/subsystems/Subsystem.java
+++ src/main/java/frc/robot/subsystems/Subsystem.java
@@ -9,13 +9,12 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /**
  * TODO: Create a field (instance) variable and call it something like "testValue".
- * Also write an accessor method. Increment testValue by 0.1 continuously.
+ * Also write an accessor method.
  */
 public class Subsystem extends SubsystemBase 
 {
 	private static Subsystem instance;
 	/** Creates a new ExampleSubsystem. */
-
 
 
 	private Subsystem() {}
@@ -26,6 +25,7 @@ public class Subsystem extends SubsystemBase
 	@Override
 	public void periodic() 
 	{
+		// TODO: increment testValue by 0.1
 	}
 
 	// This method will be called once per scheduler run during simulation
